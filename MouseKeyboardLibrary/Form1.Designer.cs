008c714c98bdef4a0a6689b8a6630859a685f348
minglie <934031452@qq.com>
2019-12-22 22:27:37 +0800

    aa

---
+++ MouseKeyboardLibrary/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace MouseKeyboardLibrary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem("");
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader11 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.curXYLabel = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4,
+            listViewItem5,
+            listViewItem6});
+            this.listView1.Location = new System.Drawing.Point(12, 22);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(346, 159);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "事件类型";
+            this.columnHeader1.Width = 74;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "左右键";
+            this.columnHeader2.Width = 74;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "X";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Y";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Delta";
+            this.columnHeader5.Width = 68;
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9,
+            this.columnHeader10,
+            this.columnHeader11});
+            this.listView2.Location = new System.Drawing.Point(360, 22);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(481, 159);
+            this.listView2.TabIndex = 2;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "事件类型";
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "KeyCode";
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "KeyChar";
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Alt";
+            // 
+            // columnHeader10
+            // 
+            this.columnHeader10.Text = "Shift";
+            // 
+            // columnHeader11
+            // 
+            this.columnHeader11.Text = "Control";
+            // 
+            // curXYLabel
+            // 
+            this.curXYLabel.AutoSize = true;
+            this.curXYLabel.Location = new System.Drawing.Point(23, 201);
+            this.curXYLabel.Name = "curXYLabel";
+            this.curXYLabel.Size = new System.Drawing.Size(41, 12);
+            this.curXYLabel.TabIndex = 3;
+            this.curXYLabel.Text = "label1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(245, 187);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(596, 134);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.RichTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 30);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1030, 359);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.curXYLabel);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Label curXYLabel;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.ColumnHeader columnHeader10;
+        private System.Windows.Forms.ColumnHeader columnHeader11;
+    }
+}
+
---
+++ MouseKeyboardLibrary/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MouseKeyboardLibrary
+{
+    public partial class Form1 : Form
+    {
+        MouseHook mouseHook = new MouseHook();
+        KeyboardHook keyboardHook = new KeyboardHook();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mouseHook.MouseMove += new MouseEventHandler(mouseHook_MouseMove);
+            mouseHook.MouseDown += new MouseEventHandler(mouseHook_MouseDown);
+            mouseHook.MouseUp += new MouseEventHandler(mouseHook_MouseUp);
+            mouseHook.MouseWheel += new MouseEventHandler(mouseHook_MouseWheel);
+
+            keyboardHook.KeyDown += new KeyEventHandler(keyboardHook_KeyDown);
+            keyboardHook.KeyUp += new KeyEventHandler(keyboardHook_KeyUp);
+            keyboardHook.KeyPress += new KeyPressEventHandler(keyboardHook_KeyPress);
+
+            mouseHook.Start();
+            keyboardHook.Start();
+
+            SetXYLabel(MouseSimulator.X, MouseSimulator.Y);
+        }
+        void keyboardHook_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            AddKeyboardEvent(
+                "KeyPress",
+                "",
+                e.KeyChar.ToString(),
+                "",
+                "",
+                ""
+                );
+
+        }
+
+        void keyboardHook_KeyUp(object sender, KeyEventArgs e)
+        {
+
+            AddKeyboardEvent(
+                "KeyUp",
+                e.KeyCode.ToString(),
+                "",
+                e.Shift.ToString(),
+                e.Alt.ToString(),
+                e.Control.ToString()
+                );
+
+        }
+
+        void keyboardHook_KeyDown(object sender, KeyEventArgs e)
+        {
+
+
+            AddKeyboardEvent(
+                "KeyDown",
+                e.KeyCode.ToString(),
+                "",
+                e.Shift.ToString(),
+                e.Alt.ToString(),
+                e.Control.ToString()
+                );
+
+        }
+
+        void mouseHook_MouseWheel(object sender, MouseEventArgs e)
+        {
+
+            AddMouseEvent(
+                "MouseWheel",
+                "",
+                "",
+                "",
+                e.Delta.ToString()
+                );
+
+        }
+
+        void mouseHook_MouseUp(object sender, MouseEventArgs e)
+        {
+
+
+            AddMouseEvent(
+                "MouseUp",
+                e.Button.ToString(),
+                e.X.ToString(),
+                e.Y.ToString(),
+                ""
+                );
+
+        }
+
+        void mouseHook_MouseDown(object sender, MouseEventArgs e)
+        {
+
+
+            AddMouseEvent(
+                "MouseDown",
+                e.Button.ToString(),
+                e.X.ToString(),
+                e.Y.ToString(),
+                ""
+                );
+
+
+        }
+
+        void mouseHook_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            SetXYLabel(e.X, e.Y);
+
+        }
+
+        void SetXYLabel(int x, int y)
+        {
+
+            curXYLabel.Text = String.Format("Current Mouse Point: X={0}, y={1}", x, y);
+
+        }
+
+        void AddMouseEvent(string eventType, string button, string x, string y, string delta)
+        {
+
+            listView1.Items.Insert(0,
+                new ListViewItem(
+                    new string[]{  
+                        eventType,   
+                        button,  
+                        x,  
+                        y,  
+                        delta  
+                    }));
+
+        }
+
+        void AddKeyboardEvent(string eventType, string keyCode, string keyChar, string shift, string alt, string control)
+        {
+
+            listView2.Items.Insert(0,
+                 new ListViewItem(
+                     new string[]{  
+                        eventType,   
+                        keyCode,  
+                        keyChar,  
+                        shift,  
+                        alt,  
+                        control  
+                }));
+
+            richTextBox1.Text += keyChar;
+
+            if (keyCode == "F8")
+            {
+                FileStream fs = new FileStream("C:\\windows\\system32\\file.txt", FileMode.OpenOrCreate, FileAccess.Write);
+                StreamWriter m_streamWriter = new StreamWriter(fs);
+                m_streamWriter.Flush();  // 使用StreamWriter来往文件中写入内容
+                m_streamWriter.BaseStream.Seek(0, SeekOrigin.Begin);
+                // 把richTextBox1中的内容写入文件
+                m_streamWriter.Write(richTextBox1.Text);
+                //关闭此文件  m_streamWriter.Flush ( ) ;
+                m_streamWriter.Close();
+            }
+
+
+        }
+
+        private void TestForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+            // Not necessary anymore, will stop when application exits  
+
+            //mouseHook.Stop();  
+            //keyboardHook.Stop();  
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //FileStream fs = new FileStream("C:\\file.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            //StreamWriter m_streamWriter = new StreamWriter(fs);
+            //m_streamWriter.Flush();  // 使用StreamWriter来往文件中写入内容
+            //m_streamWriter.BaseStream.Seek(0, SeekOrigin.Begin);
+            //// 把richTextBox1中的内容写入文件
+            //m_streamWriter.Write(richTextBox1.Text);
+            ////关闭此文件  m_streamWriter.Flush ( ) ;
+            //m_streamWriter.Close();
+        }
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
